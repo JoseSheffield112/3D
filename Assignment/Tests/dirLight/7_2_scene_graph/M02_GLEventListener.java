f707eafb9444e79972387a86db35be3d5077d51a
JosePBAlves <jalves1@sheffield.ac.uk>
2021-11-07 20:23:14 +0000

    dirLight works with 1 light!

---
+++ Assignment/Tests/dirLight/7_2_scene_graph/M02_GLEventListener.java
@@ -0,0 +1,137 @@
+import gmaths.*;
+
+import java.nio.*;
+import com.jogamp.common.nio.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.util.*;
+import com.jogamp.opengl.util.awt.*;
+import com.jogamp.opengl.util.glsl.*;
+  
+public class M02_GLEventListener implements GLEventListener {
+  
+  private static final boolean DISPLAY_SHADERS = false;
+    
+  public M02_GLEventListener(Camera camera) {
+    this.camera = camera;
+    this.camera.setPosition(new Vec3(4f,8f,18f));
+    this.camera.setTarget(new Vec3(0f,2f,0f));
+  }
+  
+  // ***************************************************
+  /*
+   * METHODS DEFINED BY GLEventListener
+   */
+
+  /* Initialisation */
+  public void init(GLAutoDrawable drawable) {   
+    GL3 gl = drawable.getGL().getGL3();
+    System.err.println("Chosen GLCapabilities: " + drawable.getChosenGLCapabilities());
+    gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
+    gl.glClearDepth(1.0f);
+    gl.glEnable(GL.GL_DEPTH_TEST);
+    gl.glDepthFunc(GL.GL_LESS);
+    gl.glFrontFace(GL.GL_CCW);    // default is 'CCW'
+    gl.glEnable(GL.GL_CULL_FACE); // default is 'not enabled'
+    gl.glCullFace(GL.GL_BACK);   // default is 'back', assuming CCW
+    initialise(gl);
+    startTime = getSeconds();
+  }
+  
+  /* Called to indicate the drawing surface has been moved and/or resized  */
+  public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    GL3 gl = drawable.getGL().getGL3();
+    gl.glViewport(x, y, width, height);
+    float aspect = (float)width/(float)height;
+    camera.setPerspectiveMatrix(Mat4Transform.perspective(45, aspect));
+  }
+
+  /* Draw */
+  public void display(GLAutoDrawable drawable) {
+    GL3 gl = drawable.getGL().getGL3();
+    render(gl);
+  }
+
+  /* Clean up memory, if necessary */
+  public void dispose(GLAutoDrawable drawable) {
+    GL3 gl = drawable.getGL().getGL3();
+    light.dispose(gl);
+    light2.dispose(gl);
+    floor.dispose(gl);
+  }
+
+  // ***************************************************
+  /* THE SCENE
+   * Now define all the methods to handle the scene.
+   * This will be added to in later examples.
+   */
+
+  private Camera camera;
+  private Mat4 perspective;
+  private Model floor, cube;
+  private Light light, light2;
+
+  private void initialise(GL3 gl) {
+    createRandomNumbers();
+	
+	// loading tectures!
+    int[] textureId0 = TextureLibrary.loadTexture(gl, "textures/chequerboard.jpg");
+    int[] textureId1 = TextureLibrary.loadTexture(gl, "textures/container2.jpg");
+    int[] textureId2 = TextureLibrary.loadTexture(gl, "textures/container2_specular.jpg");
+    
+    light = new Light(gl, 1f);
+    light2 = new Light(gl, 0.5f);
+    light.setCamera(camera);
+    light2.setCamera(camera);
+    // an array with the light
+    Light[] lights = {light, light2};
+	
+    //Setting up model for our wall!
+    Mesh mesh = new Mesh(gl, TwoTriangles.vertices.clone(), TwoTriangles.indices.clone());
+    Shader shader = new Shader(gl, "vs_tt_05.txt", "fs_tt_05.txt");
+    Material material = new Material(new Vec3(0.0f, 0.5f, 0.81f), new Vec3(0.0f, 0.5f, 0.81f), new Vec3(0.3f, 0.3f, 0.3f), 32.0f);
+    Mat4 modelMatrix = Mat4Transform.scale(16,1f,16);
+    floor = new Model(gl, camera, lights, shader, material, modelMatrix, mesh, textureId0);
+  }
+ 
+  private void render(GL3 gl) {
+    gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+    light.render(gl);
+    light2.render(gl);
+    floor.render(gl);
+  }
+  
+  // The light's position is continually being changed, so needs to be calculated for each frame.
+  private Vec3 getLightPosition() {
+    double elapsedTime = getSeconds()-startTime;
+    float x = 5.0f*(float)(Math.sin(Math.toRadians(elapsedTime*50)));
+    float y = 2.7f;
+    float z = 5.0f*(float)(Math.cos(Math.toRadians(elapsedTime*50)));
+    return new Vec3(x,y,z);   
+    //return new Vec3(5f,3.4f,5f);
+  }
+  
+  // ***************************************************
+  /* TIME
+   */ 
+  
+  private double startTime;
+  
+  private double getSeconds() {
+    return System.currentTimeMillis()/1000.0;
+  }
+
+  // ***************************************************
+  /* An array of random numbers
+   */ 
+  
+  private int NUM_RANDOMS = 1000;
+  private float[] randoms;
+  
+  private void createRandomNumbers() {
+    randoms = new float[NUM_RANDOMS];
+    for (int i=0; i<NUM_RANDOMS; ++i) {
+      randoms[i] = (float)Math.random();
+    }
+  }
+  
+}
